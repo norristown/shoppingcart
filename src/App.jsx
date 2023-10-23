726e31f9cbcacd452ab5597704bce9a17e1501f8
norristown <jarvasity@gmail.com>
2023-10-23 00:02:46 -0400

    gettin started

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [storeItems, setStoreItems] = useState([
+    {
+      title: "",
+      category: "",
+      description: "",
+      id: "",
+      image: "",
+      price: "",
+      rating: { rating: "", count: "" },
+    },
+  ]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const url = await fetch("https://fakestoreapi.com/products");
+      const data = await url.json();
+      setStoreItems(data);
+    }
+    getProducts();
+  });
   return (
     <>
       <p>Hello</p>
