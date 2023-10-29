f1a380eb7cc878d57f66ed2b1aab245d65830dd8
norristown <jarvasity@gmail.com>
2023-10-29 04:17:26 -0400

    added remove item from cart and if no items in cart, it displays cart is empty message

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,52 +1,65 @@
 import PageNav from "../components/PageNav";
 
 function Cart({cartItems, onSetCartItems}) {
+  function handleRemoveCartItem(id) {
+
+    onSetCartItems(prev => prev.map(item => item.id === id ? 
+      { ...item, quantity: item.quantity - 1 } : 
+      item ))
+
+    onSetCartItems(prev => prev.filter(item => item.quantity !== 0))
+
+  }
   return (
     <div>
       <PageNav />
       <h1>Cart</h1>
-      
-      <div  style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(4, 1fr)",
-        gridGap: "2rem",
-        fontFamily: "sans-serif",
-      }}>
-      {cartItems.map((x) => (
-        <div
-          key={x.id}
-          style={{
-            border: "3px solid black",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div>
-            <p>Id: {x.id}</p>
-            <p>Title: {x.title}</p>
-            <p>Quantity: {x.quantity}</p>
-            <p>Price: {x.price * x.quantity}</p>
-     
-          </div>
-
-          <img
-            src={x.image}
-            style={{ height: "auto", width: "100px", marginBottom: "2rem" }}
-            alt="product"
-          />
-          <div style={{ display: "flex", flexDirection: "column" }}>
+      {cartItems.length === 0 ? <div>Your Cart Is Empty</div> : 
+        <div  style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gridGap: "2rem",
+          fontFamily: "sans-serif",
+        }}>
+        {cartItems.map((x) => (
+          x.quantity === 0 ? null : 
+          <div
+            key={x.id}
+            style={{
+              border: "3px solid black",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            
             <div>
-              <button>-</button>
-              <span></span>
-              <button>+</button>
+              <p>Id: {x.id}</p>
+              <p>Title: {x.title}</p>
+              <p>Quantity: {x.quantity}</p>
+              <p>Price: {x.price * x.quantity}</p>
+       
             </div>
-           
+  
+            <img
+              src={x.image}
+              style={{ height: "auto", width: "100px", marginBottom: "2rem" }}
+              alt="product"
+            />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div>
+                <button onClick={() => handleRemoveCartItem(x.id)}>-</button>
+                <span></span>
+                <button>+</button>
+              </div>
+             
+            </div>
           </div>
+        ))}
         </div>
-      ))}
-      </div>
+      }
+      
     </div>
   );
 }
